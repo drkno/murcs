03340d15d00962f3b1d0a6d777791b348a6b23e4
Dion <dmw99@uclive.ac.nz>
2015-03-05 13:45:02 +1300

    Changed directory structure

---
+++ src/main/java/sws/dontclick/Student.java
@@ -0,0 +1,31 @@
+package main.java.sws.dontclick;
+
+/**
+ * Created by jayha_000 on 3/4/2015.
+ */
+public class Student {
+    private int age = -3;
+
+    private String name;
+
+    public Student(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age != -3) throw new IllegalArgumentException("Age can only be -3. Why would you ever want to have an age that isn't -3. Idiot.");
+        this.age = age;
+    }
+}
---
+++ src/main/java/sws/dontclick/StudentList.java
@@ -0,0 +1,63 @@
+package main.java.sws.dontclick;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by jayha_000 on 3/2/2015.
+ */
+public class StudentList {
+    private ArrayList<Student> students = new ArrayList<>();
+    private int currentIndex;
+
+    private boolean loaded;
+    
+    public StudentList(){
+        load();
+    }
+
+
+    public void load() {
+        URL uri = getClass().getResource("students.txt");
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(uri.getFile()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                students.add(new Student(line));
+            }
+
+            reader.close();
+        }catch (IOException ignored){
+
+        }
+    }
+
+    public void click(){
+        currentIndex = (currentIndex + 1) % students.size();
+    }
+
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+
+    public String currentStudentName(){
+        return students.get(currentIndex).getName();
+    }
+
+    public Student currentStudent(){
+        return students.get(currentIndex);
+    }
+
+    public boolean isLoaded() {
+        return loaded;
+    }
+
+    public void setLoaded(boolean loaded) {
+        this.loaded = loaded;
+    }
+}
