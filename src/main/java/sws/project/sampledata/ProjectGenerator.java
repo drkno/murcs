01ba7cc369cb504cdae42e35371d05f72108675c
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 16:16:28 +1300

    Adds a project generator class Issue: 31

---
+++ src/main/java/sws/project/sampledata/ProjectGenerator.java
@@ -0,0 +1,13 @@
+package sws.project.sampledata;
+
+import sws.project.model.Project;
+
+/**
+ *
+ */
+public class ProjectGenerator implements Generator<Project> {
+    @Override
+    public Project generate() {
+        return null;
+    }
+}
