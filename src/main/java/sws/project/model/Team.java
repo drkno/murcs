c66a091c39c9f90e3147a12e4b35d1ea7e3537cf
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 13:54:57 +1300

    Adds more detail to the team implementation, including getters and setters and increases commenting Issue: 19

--- src/main/java/sws/project/model/Team.java
+++ src/main/java/sws/project/model/Team.java
@@ -11,19 +11,49 @@ import java.util.ArrayList;
 
 public class Team extends Model{
     private Project project;
-
-    private String shortName;
-    private String longName;
-
     private String description;
 
-    private ArrayList<Role> members = new ArrayList<>();
+    private ArrayList<Person> members = new ArrayList<>();
 
-    public void addTeamMember(Person person){
-        //TODO generate default role and add person to team
+    /**
+     * Gets the project the team is associated with
+     * @return The teams project
+     */
+    public Project getProject() {
+        return project;
     }
 
-    public void removeTeamMember(Person person){
-        members.remove(person);
+    /**
+     * Sets the project the team is associated with
+     * @param project The new project for the team to work on
+     */
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    /**
+     * Returns a list of members in the team. Following the
+     * Java method of adding to lists, the preferred method
+     * of adding is to use getMembers().add(person);
+     * @return A list of the team members
+     */
+    public ArrayList<Person> getMembers() {
+        return members;
+    }
+
+    /**
+     * A description of the team
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the team
+     * @param description the new description
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
