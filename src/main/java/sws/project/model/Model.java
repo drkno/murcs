dd5d9feaee7b916de6229c7142548b8e8f7f443e
Matthew <mrk45@uclive.ac.nz>
2015-03-11 14:25:31 +1300

    Add javadoc to the Model class. Issue: 18

--- src/main/java/sws/project/model/Model.java
+++ src/main/java/sws/project/model/Model.java
@@ -1,26 +1,43 @@
 package sws.project.model;
 
 /**
- *
+ * Contains the basic model for each object type.
  */
 public abstract class Model {
     private String shortName;
     private String longName;
 
+    /**
+     * Gets the short name.
+     * @return the short name.
+     */
     public String getShortName() {
         return shortName;
     }
 
+    /**
+     * Sets the short name.
+     * @param shortName the new short name.
+     * @throws java.lang.IllegalArgumentException if the shortName is invalid
+     */
     public void setShortName(String shortName) {
         if (shortName == null) throw new IllegalArgumentException("shortname cannot be null!");
 
         this.shortName = shortName;
     }
 
+    /**
+     * Gets the long name.
+     * @return the long name.
+     */
     public String getLongName() {
         return longName;
     }
 
+    /**
+     * Sets the long name.
+     * @param longName the new long name
+     */
     public void setLongName(String longName) {
         this.longName = longName;
     }
