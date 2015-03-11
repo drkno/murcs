0f95984964f7afc7f4f43bc1df27b1c4a809e20e
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 16:15:35 +1300

    Makes TeamGenerator more robust (thanks Daniel) Issue: 31

--- src/main/java/sws/project/sampledata/TeamGenerator.java
+++ src/main/java/sws/project/sampledata/TeamGenerator.java
@@ -10,39 +10,57 @@ import java.util.ArrayList;
  *
  */
 public class TeamGenerator implements Generator<Team> {
+    private String[] teamNames = new String[]{"Foo", "Bar", "New team", "SENGineers", "Fred's Team"};
+    private String[] descriptions = new String[]{NameGenerator.getLoremIpsum()};
+
+    private float probOfScrumMaster = 0.5f;
+    private float probOfProductOwner = 0.5f;
+
     private Generator<Person> personGenerator;
 
     public TeamGenerator(){
         personGenerator = new PersonGenerator();
     }
 
-    public TeamGenerator(Generator<Person> personGenerator){
+    public TeamGenerator(Generator<Person> personGenerator, String[] teamNames, String[] descriptions, float probOfProductOwner, float probOfScrumMaster){
         this.personGenerator = personGenerator;
+        this.teamNames = teamNames;
+        this.descriptions = descriptions;
+
+        this.probOfProductOwner = probOfProductOwner;
+        this.probOfScrumMaster = probOfScrumMaster;
     }
 
     @Override
     public Team generate() {
         Team team = new Team();
 
-        String shortName = NameGenerator.randomString(10);
+        String shortName = NameGenerator.randomElement(teamNames);
         String longName = shortName + NameGenerator.random(10);
 
-        String description = NameGenerator.getLoremIpsum();
+        String description = NameGenerator.randomElement(descriptions);
 
         Person productOwner = null;
         Person scrumMaster = null;
 
-        int memberCount = NameGenerator.random(3, 9);
+        int memberCount = NameGenerator.random(3, 10);
+
         ArrayList<Person> members = new ArrayList<>();
         for (int i = 0; i < memberCount; ++i){
             Person p = personGenerator.generate();
-            if (NameGenerator.random() < 2/memberCount)
+            if (NameGenerator.random() < probOfProductOwner/memberCount)
                 productOwner = p;
-            if (NameGenerator.random() < 2/memberCount)
+            if (NameGenerator.random() < probOfScrumMaster/memberCount)
                 scrumMaster = p;
 
             members.add(p);
         }
+
+        if (probOfProductOwner == 1)
+            productOwner = members.get(0);
+
+        if (probOfScrumMaster == 1)
+            scrumMaster = members.get(1);
 
         team.setShortName(shortName);
         team.setLongName(longName);
