a201511ab97c90155f23dd2c51820a5e47f9f302
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 16:22:10 +1300

    Implements the project generator Issue: 31

--- src/main/java/sws/project/sampledata/ProjectGenerator.java
+++ src/main/java/sws/project/sampledata/ProjectGenerator.java
@@ -1,13 +1,53 @@
 package sws.project.sampledata;
 
 import sws.project.model.Project;
+import sws.project.model.Team;
+
+import java.util.ArrayList;
 
 /**
  *
  */
 public class ProjectGenerator implements Generator<Project> {
+    private String[] projectNames = new String[]{"A project", "Something exciting"};
+    private String[] descriptions = new String[]{"A very exciting description", NameGenerator.getLoremIpsum()};
+
+    private Generator<Team> teamGenerator;
+
+    public ProjectGenerator(){
+        teamGenerator = new TeamGenerator();
+    }
+
+    public ProjectGenerator(Generator<Team> teamGenerator, String[] projectNames, String[] descriptions){
+        this.teamGenerator = teamGenerator;
+        this.projectNames = projectNames;
+        this.descriptions = descriptions;
+    }
+
     @Override
     public Project generate() {
-        return null;
+        Project project = new Project();
+
+        String shortName = NameGenerator.randomElement(projectNames);
+        String longName = NameGenerator.randomString(1000);
+
+        String description = NameGenerator.randomElement(descriptions);
+
+        int teamCount = NameGenerator.random(1, 50);
+        ArrayList<Team> teams = new ArrayList<>();
+
+        for (int i = 0; i < teamCount; ++i){
+            Team team = teamGenerator.generate();
+            teams.add(team);
+        }
+
+        project.setShortName(shortName);
+        project.setLongName(longName);
+
+        project.setDescription(description);
+
+        project.getTeams().addAll(teams);
+
+        return project;
     }
 }
