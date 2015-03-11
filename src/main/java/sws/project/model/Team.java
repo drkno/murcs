2e61186c4040d8d67c3202f3892144e765b0dd92
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 13:29:01 +1300

    Updates the 'Team' class to reflect the new way People are Associated with projects (through their team) Issue: 19

--- src/main/java/sws/project/model/Team.java
+++ src/main/java/sws/project/model/Team.java
@@ -19,16 +19,10 @@ public class Team extends Model{
     private ArrayList<Role> members = new ArrayList<>();
 
     public void addTeamMember(Person person){
-        assertOnProject(person);
-
         //TODO generate default role and add person to team
     }
 
     public void removeTeamMember(Person person){
         members.remove(person);
-    }
-
-    private void assertOnProject(Person person){
-        throw new NotImplementedException();
     }
 }
