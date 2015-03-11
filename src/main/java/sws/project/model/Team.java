a013176287cc57c20e97d2c5e9822987064e2a4b
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-11 13:19:19 +1300

    Creates *very* basic implementation of Team. Issue: 19

--- src/main/java/sws/project/model/Team.java
+++ src/main/java/sws/project/model/Team.java
@@ -1,7 +1,34 @@
 package sws.project.model;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
 /**
  *
  */
 
 public class Team {
+    private Project project;
+
+    private String shortName;
+    private String longName;
+
+    private String description;
+
+    private ArrayList<Role> members = new ArrayList<>();
+
+    public void addTeamMember(Person person){
+        assertOnProject(person);
+
+        //TODO generate default role and add person to team
+    }
+
+    public void removeTeamMember(Person person){
+        members.remove(person);
+    }
+
+    private void assertOnProject(Person person){
+        throw new NotImplementedException();
+    }
 }
