9f56628d06b2dff00d5444ef47bfb55c6d4a4d76
Dion <dmw99@uclive.ac.nz>
2015-03-17 15:15:20 +1300

    Fixed broken test, in createprojectstepdefs

--- src/test/java/sws/project/acceptance/stepdefs/CreateProjectStepDefs.java
+++ src/test/java/sws/project/acceptance/stepdefs/CreateProjectStepDefs.java
@@ -3,7 +3,6 @@ package sws.project.acceptance.stepdefs;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import javafx.stage.Stage;
@@ -19,9 +18,11 @@ import org.testfx.framework.junit.ApplicationTest;
 public class CreateProjectStepDefs extends ApplicationTest {
 
     private FxRobot fx;
+    private Stage primaryStage;
 
     @Before
     public void setUp() throws Exception {
+        primaryStage = FxToolkit.registerPrimaryStage();
         FxToolkit.setupApplication(sws.project.view.App.class);
         fx = new FxRobot();
         launch(sws.project.view.App.class);
