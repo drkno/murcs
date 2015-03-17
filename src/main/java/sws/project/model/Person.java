302dfc7bac5311fb05075a54209679d8c6969b35
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-18 11:51:29 +1300

    Fixes more formatting (you happy Dion?)

--- src/main/java/sws/project/model/Person.java
+++ src/main/java/sws/project/model/Person.java
@@ -104,8 +104,7 @@ public class Person extends Model {
 
         Person other = (Person)object;
 
-        if (other.getUserId() != null && other.getUserId().equals(getUserId())) return true;
+        return other.getUserId() != null && other.getUserId().equals(getUserId());
 
-        return false;
     }
 }
