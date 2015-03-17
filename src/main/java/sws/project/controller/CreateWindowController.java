b780de7869ce189d53f6ed92d75f83fb44f17b85
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-17 20:10:40 +1300

    Adds more documentation to the CreateWindowController

--- src/main/java/sws/project/controller/CreateWindowController.java
+++ src/main/java/sws/project/controller/CreateWindowController.java
@@ -44,18 +44,37 @@ public class CreateWindowController {
         stage.close();
     }
 
+    /**
+     * Sets the method that is called when cancel is clicked
+     * @param cancelClicked The method to call when cancel is clicked
+     */
     public void setCancelClicked(Callable<Void> cancelClicked) {
         this.cancelClicked = cancelClicked;
     }
 
+    /**
+     * Sets the method that is called when okay is clicked
+     * @param okayClicked The callable
+     */
     public void setOkayClicked(Callable<Void> okayClicked) {
         this.okayClicked = okayClicked;
     }
 
+    /**
+     * Sets the content of the form
+     * @param content The form
+     */
     public void setContent(Node content){
         contentPane.getChildren().add(content);
     }
 
+    /**
+     * Creates a new form for with the 'content' node as it's content
+     * @param content The content
+     * @param okayClicked The okay callback
+     * @param cancelClicked The cancel callback
+     * @return The form
+     */
     public static Parent newCreateNode(Node content, Callable<Void> okayClicked, Callable<Void> cancelClicked){
         try {
             FXMLLoader loader = new FXMLLoader(CreateWindowController.class.getResource("/sws/project/CreatorWindow.fxml"));
