cbda553e369c14788421b3f07d1490759300d1e5
Fallacious Reasoning <jay.harris@outlook.co.nz>
2015-03-17 15:58:12 +1300

    Adds documentation for the ModelTypes enum and a new helper function for getting the model type from a type

--- src/main/java/sws/project/controller/ModelTypes.java
+++ src/main/java/sws/project/controller/ModelTypes.java
@@ -1,9 +1,10 @@
 package sws.project.controller;
 
-import java.util.Observable;
+import sws.project.model.Model;
 
 /**
- * Created by James on 13/03/2015.
+ * A enum of the different types of model that
+ * are in the application
  */
 public enum ModelTypes {
 
@@ -12,7 +13,34 @@ public enum ModelTypes {
     Team,
     Skills;
 
+    /**
+     * Gets the model type from an index
+     * 0: Project
+     * 1: People
+     * 2: Team
+     * 3: Skills
+     * @param index The index
+     * @return The model type
+     */
     public static ModelTypes getModelType(int index) {
         return values()[index];
+    }
+
+    /**
+     * Gets the model type from a model object
+     * @param clazz The type of the model object
+     * @return
+     */
+    public static ModelTypes getModelType(Class<? extends Model> clazz){
+        if (clazz == sws.project.model.Project.class)
+            return Project;
+        else if (clazz == sws.project.model.Team.class)
+            return Team;
+        else if (clazz == sws.project.model.Person.class)
+            return People;
+        else if (clazz == sws.project.model.Skill.class)
+            return Skills;
+
+        throw new IllegalArgumentException("Clazz " + clazz.getName() + " is not supported");
     }
 }
