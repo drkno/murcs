b2fab3cec8a72cbc5ef843c51cc252f49a6b8795
Matthew <mrk45@uclive.ac.nz>
2015-03-18 12:51:13 +1300

    Comments for PR. Issue: ?

--- src/main/java/sws/project/magic/tracking/TrackingTask.java
+++ src/main/java/sws/project/magic/tracking/TrackingTask.java
@@ -22,11 +22,13 @@ public class TrackingTask extends TimerTask {
 
     @Override
     public void run() {
+        // notify each of the listeners
         for (int i = 0; i < _listeners.size(); i++) {
             try {
                 _listeners.get(i).run(_change);
             }
             catch (Exception e) {
+                // remove listeners that don't exist.
                 _listeners.remove(i);
                 i--;
             }
