6a1f01b78677fcb84636abe0f2988b64d6bcf864
Matthew <mrk45@uclive.ac.nz>
2015-03-18 18:48:16 +1300

    Add missing JavaDoc. Issue: bugfix/techdebt

--- src/main/java/sws/project/controller/ModelTypes.java
+++ src/main/java/sws/project/controller/ModelTypes.java
@@ -1,17 +1,19 @@
 package sws.project.controller;
 
-import java.util.Observable;
-
 /**
- * Created by James on 13/03/2015.
+ * Enumerates each of the types that can be displayed in the sidebar list.
  */
 public enum ModelTypes {
-
     Project,
     People,
     Team,
     Skills;
 
+    /**
+     * Gets the model type for a provided index.
+     * @param index index to get model type for.
+     * @return ModelType equivalent.
+     */
     public static ModelTypes getModelType(int index) {
         return values()[index];
     }
