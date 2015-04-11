104d9489261b83860c8884e5bef18d40dd1ff73d
Matthew <mrk45@uclive.ac.nz>
2015-04-11 12:02:54 +1200

    Adds javadoc to CustomException.
    Issue:bugfix
    story:130

--- src/main/java/sws/murcs/exceptions/CustomException.java
+++ src/main/java/sws/murcs/exceptions/CustomException.java
@@ -5,23 +5,46 @@ package sws.murcs.exceptions;
  * that all custom exceptions can be caught whilst ignoring built in exceptions.
  */
 public abstract class CustomException extends Exception {
+    /**
+     * Instantiates a new CustomException.
+     */
     public CustomException() {
         super();
     }
 
+    /**
+     * Instantiates a new CustomException.
+     * @param message exception message to use.
+     */
     public CustomException(String message) {
         super(message);
     }
 
+    /**
+     * Instantiates a new CustomException.
+     * @param cause exception that caused this exception.
+     */
     public CustomException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Instantiates a new CustomException.
+     * @param message exception message to use.
+     * @param cause exception that caused this exception.
+     */
     public CustomException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public CustomException(String message, Throwable cause, boolean enableSuppession, boolean writableStackTrace) {
-        super(message, cause, enableSuppession, writableStackTrace);
+    /**
+     * Instantiates a new CustomException.
+     * @param message exception message to use.
+     * @param cause exception that caused this exception.
+     * @param enableSuppression ignore when thrown inside a finally block.
+     * @param writableStackTrace whether or not a stack trace should be writable.
+     */
+    public CustomException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
