8a2d0832a140ac8e207a499a3754d34b0011fe09
James <jaf138@uclive.ac.nz>
2015-04-15 09:42:26 +1200

    #implement The Release model

---
+++ src/main/java/sws/murcs/model/Release.java
@@ -0,0 +1,61 @@
+package sws.murcs.model;
+
+import java.time.LocalDate;
+
+/**
+ * A model that represents a release for a project.
+ */
+public class Release extends Model {
+
+    private String description;
+    private LocalDate releaseDate;
+    private Project associatedProject;
+
+    /**
+     * Gets the release date for the release.
+     * @return The release date
+     */
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    /**
+     * Set the release date for the release
+     * @param releaseDate The release date
+     */
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     * Gets the description for the release.
+     * @return The description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the release.
+     * @param description The description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets the Project that the release is associated with.
+     * @return The associated project
+     */
+    public Project getAssociatedProject() {
+        return associatedProject;
+    }
+
+    /**
+     * Sets the project that the release is associated with.
+     * @param associatedProject The associated project
+     */
+    public void setAssociatedProject(Project associatedProject) {
+        this.associatedProject = associatedProject;
+    }
+}
