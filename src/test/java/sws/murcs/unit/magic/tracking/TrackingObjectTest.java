08d196635293a69fd2a7ad7902a6fab04ffc7b28
Matthew <mrk45@uclive.ac.nz>
2015-04-20 11:16:25 +1200

    Fix a use-after-free test failure spillover due to TestFX not cleaning up after itself.
    Issue:feature
    story:119

--- src/test/java/sws/murcs/unit/magic/tracking/TrackingObjectTest.java
+++ src/test/java/sws/murcs/unit/magic/tracking/TrackingObjectTest.java
@@ -1,12 +1,13 @@
 package sws.murcs.unit.magic.tracking;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import sws.murcs.magic.tracking.TrackableObject;
 import sws.murcs.magic.tracking.TrackableValue;
 import sws.murcs.magic.tracking.UndoRedoManager;
+import sws.murcs.magic.tracking.listener.ChangeListenerHandler;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class TrackingObjectTest {
     public class TestObject {
@@ -38,8 +39,18 @@ public class TrackingObjectTest {
         }
     }
 
+    private static Field listenersField;
+
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        listenersField = UndoRedoManager.class.getDeclaredField("changeListeners");
+        listenersField.setAccessible(true);
+    }
+
     @Before
-    public void setup() {
+    public void setup() throws IllegalAccessException {
+        UndoRedoManager.forget(true);
+        listenersField.set(null, new ArrayList<ChangeListenerHandler>());
         UndoRedoManager.setMaximumCommits(-1);
     }
 
