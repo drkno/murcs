aa40c8969ec48e701c56a42d46bef61ef3a42063
Dion Woolley <dmw99@uclive.ac.nz>
2015-05-04 12:53:08 +1200

    Remove unused package

--- src/main/java/sws/murcs/magic/easyedit/fxml/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains the controllers for some of the
- * generated fxml files as well as the fxml pane generator.
- */
-package sws.murcs.magic.easyedit.fxml;
--- src/main/java/sws/murcs/magic/easyedit/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains all the classes for the
- * editing using reflection (currently unused).
- */
-package sws.murcs.magic.easyedit;
