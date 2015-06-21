4c766bef4606d7179e910deacb795112b6686451
Jay Harris <joh19@uclive.ac.nz>
2015-06-21 20:17:51 +1200

    Created model object for AcceptanceCondition containing a description

---
+++ src/main/java/sws/murcs/model/AcceptanceCondition.java
@@ -0,0 +1,29 @@
+package sws.murcs.model;
+
+/**
+ * A class representing a single
+ * acceptance condition on a story.
+ */
+public class AcceptanceCondition {
+    /**
+     * The text representing the condition
+     */
+    private String condition;
+
+    /**
+     * Gets a string describing this condition
+     * @return The condition
+     */
+    public String getCondition() {
+        return condition;
+    }
+
+    /**
+     * Sets the condition that this acceptance condition
+     * describes
+     * @param condition The new condition
+     */
+    public void setCondition(final String condition) {
+        this.condition = condition;
+    }
+}
