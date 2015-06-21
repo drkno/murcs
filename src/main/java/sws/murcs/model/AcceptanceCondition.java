b520557cb9eface32609dba37e9cb33675134b77
Jay Harris <joh19@uclive.ac.nz>
2015-06-21 21:45:16 +1200

    Makes editing ACs undoable

--- src/main/java/sws/murcs/model/AcceptanceCondition.java
+++ src/main/java/sws/murcs/model/AcceptanceCondition.java
@@ -3,6 +3,8 @@ package sws.murcs.model;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import sws.murcs.magic.tracking.TrackableObject;
+import sws.murcs.magic.tracking.TrackableValue;
 
 /**
  * A class representing a single
@@ -10,10 +12,11 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class AcceptanceCondition {
+public class AcceptanceCondition extends TrackableObject {
     /**
      * The text representing the condition
      */
+    @TrackableValue
     private String condition;
 
     /**
@@ -31,5 +34,6 @@ public class AcceptanceCondition {
      */
     public final void setCondition(final String newCondition) {
         this.condition = newCondition;
+        commit("edit Acceptance Condition");
     }
 }
