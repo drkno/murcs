5e6894615dc91414def5db44e4630243ae67b87e
Matthew <mrk45@uclive.ac.nz>
2015-07-01 10:31:59 +1200

    #fix missing import
    story: 202

--- src/main/java/sws/murcs/controller/editor/StoryEditor.java
+++ src/main/java/sws/murcs/controller/editor/StoryEditor.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
