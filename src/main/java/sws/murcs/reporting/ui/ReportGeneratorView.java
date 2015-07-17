f546c06abcb83ffbe37b1735217f202b7eaf4a42
Dion Woolley <dmw99@uclive.ac.nz>
2015-07-17 18:51:58 +1200

    removed online fonts for offline ones

--- src/main/java/sws/murcs/reporting/ui/ReportGeneratorView.java
+++ src/main/java/sws/murcs/reporting/ui/ReportGeneratorView.java
@@ -42,9 +42,6 @@ public class ReportGeneratorView {
             }
             Scene scene = new Scene(root);
             scene.getStylesheets()
-                    .add("http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,"
-                            + "400italic,500,500italic,700,700italic,900,900italic");
-            scene.getStylesheets()
                     .add(getClass()
                             .getResource("/sws/murcs/styles/global.css")
                             .toExternalForm());
--- src/main/java/sws/murcs/view/App.java
+++ src/main/java/sws/murcs/view/App.java
@@ -143,9 +143,6 @@ public class App extends Application {
 
         Scene scene = new Scene(parent);
         scene.getStylesheets()
-                .add("http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,"
-                        + "400italic,500,500italic,700,700italic,900,900italic");
-        scene.getStylesheets()
                 .add(getClass()
                 .getResource("/sws/murcs/styles/global.css")
                 .toExternalForm());
