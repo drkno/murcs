52c5da3585d8139c8c6f49fe134fa368dcb530d1
Jay Harris <joh19@uclive.ac.nz>
2015-07-27 13:47:34 +1200

    Adds the rudiments of a GUI for editing sprints

---
+++ src/main/java/sws/murcs/controller/editor/SprintEditor.java
@@ -0,0 +1,74 @@
+package sws.murcs.controller.editor;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sws.murcs.model.*;
+import sws.murcs.model.persistence.PersistenceManager;
+
+/**
+ * The controller for editing sprints.
+ */
+public class SprintEditor extends GenericEditor<Sprint>{
+    /**
+     * Text fields for the short and long name of the sprint
+     */
+    private TextField shortNameTextField, longNamTextField;
+
+    /**
+     * A text area for the description of a sprint
+     */
+    private TextArea descriptionTextArea;
+
+    /**
+     * A combo box for picking the team associated with the sprint
+     */
+    private ComboBox<Team> teamComboBox;
+
+    /**
+     * A combobox for picking the backlog associated with a sprint
+     */
+    private ComboBox<Backlog> backlogComboBox;
+
+    /**
+     * A combobox for picking the release associated with a sprint
+     */
+    private ComboBox<Release> releaseComboBox;
+
+    @Override
+    public void loadObject() {
+        Organisation organisation = PersistenceManager.getCurrent().getCurrentModel();
+        Sprint sprint = getModel();
+
+        //Fill the short name field
+        shortNameTextField.setText(sprint.getShortName());
+
+        //Fill the long name field
+        longNamTextField.setText(sprint.getLongName());
+
+        //Fill the description field
+        //TODO descriptionTextArea.setText(sprint.getDescrription());
+
+        //Update the backlog combo box
+        backlogComboBox.getItems().clear();
+        backlogComboBox.getItems().addAll(organisation.getBacklogs());
+
+        //Update the releases combo box
+        releaseComboBox.getItems().clear();
+        releaseComboBox.getItems().addAll(organisation.getReleases());
+
+        //Update the team combo box
+        teamComboBox.getItems().clear();
+        teamComboBox.getItems().addAll(organisation.getTeams());
+    }
+
+    @Override
+    protected void saveChangesAndErrors() {
+
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+}
