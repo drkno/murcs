bbafff96b975c4e5dc67ee7a9ef2e167f98abcae
Jay Harris <jay.harris@outlook.co.nz>
2015-07-31 12:09:22 +1200

    Documents the token classes and cleans up a bit for checkstyle

--- src/main/java/sws/murcs/search/tokens/SearchToken.java
+++ src/main/java/sws/murcs/search/tokens/SearchToken.java
@@ -1,9 +1,19 @@
 package sws.murcs.search.tokens;
 
+/**
+ * Represents a single piece of a search (e.g. "Foo")
+ */
 public class SearchToken extends Token {
 
+    /**
+     * The term to search for
+     */
     private String searchTerm;
 
+    /**
+     * Creates a new search token with the specified query
+     * @param theSearchTerm The term to search for
+     */
     public SearchToken(final String theSearchTerm) {
         searchTerm = theSearchTerm;
     }
