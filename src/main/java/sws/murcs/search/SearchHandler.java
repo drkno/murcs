8732f2780501442af72e0e640a306195ccc4a718
Matthew <mrk45@uclive.ac.nz>
2015-08-01 14:03:07 +1200

    #implement search threading contract so testing can be done without mock objects
    story: 258

--- src/main/java/sws/murcs/search/SearchHandler.java
+++ src/main/java/sws/murcs/search/SearchHandler.java
@@ -1,57 +1,18 @@
 package sws.murcs.search;
 
 import edu.emory.mathcs.backport.java.util.Collections;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import sws.murcs.model.Model;
 import sws.murcs.model.Organisation;
 
+import java.util.Collection;
 import java.util.Set;
 
 public class SearchHandler {
-   /* private Set<ObservableList<SearchResult<? extends Model>>> results;
-    private boolean threadIsStarted;
-    private Thread searchThread;
-
-    private [] searchMethods = {
-            Organisation::getProjects,
-            Organisation::getStories,
-            Organisation::getReleases
-    };
 
 
-    public Set<ObservableList<SearchResult<? extends Model>>> getResults() {
-        return (Set<ObservableList<SearchResult<? extends Model>>>) Collections.unmodifiableSet(results);
+    private <T extends Model> ObservableList<SearchResult> performSearch(final Collection<T>... collections) {
+        ObservableList<SearchResult> searchResults = FXCollections.observableArrayList();
     }
-
-    public SearchHandler(String searchCriteria) {
-
-    }
-
-    private void performSearch() {
-
-    }
-
-    public void startSearch() {
-        if (threadIsStarted) {
-            return;
-        }
-        searchThread = new Thread(this::performSearch);
-        threadIsStarted = true;
-        searchThread.start();
-    }
-
-    public void stopSearch() {
-        if (!threadIsStarted) {
-            return;
-        }
-        threadIsStarted = false;
-        try {
-            searchThread.join();
-        }
-        catch (InterruptedException e) {
-            // Unlikely to happen and should not be reported as an error.
-            // This is because the outcome is what we wanted, there was
-            // just a short race that we join() ed on the wrong side of.
-        }
-    }*/
 }
--- src/main/java/sws/murcs/search/SearchResult.java
+++ src/main/java/sws/murcs/search/SearchResult.java
@@ -3,6 +3,16 @@ package sws.murcs.search;
 import sws.murcs.model.Model;
 
 public class SearchResult<T extends Model> {
+    public enum MatchStrength {
+        Strong,
+        Weak
+    }
+
     private T model;
     private String match;
+    private MatchStrength strength;
+
+    public SearchResult(T matchedModel, String match) {
+
+    }
 }
