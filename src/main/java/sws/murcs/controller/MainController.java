ac6c66598f7747879fdf1b1e2b9dd2edd765e790
Jay Harris <jay.harris@outlook.co.nz>
2015-08-20 09:31:22 +1200

    Removes solved TODO in maincontroller

--- src/main/java/sws/murcs/controller/MainController.java
+++ src/main/java/sws/murcs/controller/MainController.java
@@ -334,7 +334,6 @@ public class MainController implements UndoRedoChangeListener, ToolBarCommands, 
         accelerators.put(new KeyCodeCombination(KeyCode.Y, KeyCombination.SHORTCUT_DOWN),
                 () -> redo(null));
 
-        //TODO Work out routing for menu items
         accelerators.put(new KeyCodeCombination(KeyCode.H, KeyCombination.SHORTCUT_DOWN),
                 () -> currentTabbable.toggleSideBar(!currentTabbable.sideBarVisible()));
         accelerators.put(new KeyCodeCombination(KeyCode.EQUALS),
