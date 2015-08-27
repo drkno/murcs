616d3013b235344d3bbf80e7bda8fb0cce5f0d53
Jay Harris <jay.harris@outlook.co.nz>
2015-08-27 18:06:52 +1200

    #implement Adding effort entries (doesn't populate fields properly yet)

--- src/main/java/sws/murcs/controller/editor/EffortController.java
+++ src/main/java/sws/murcs/controller/editor/EffortController.java
@@ -1,13 +1,13 @@
 package sws.murcs.controller.editor;
 
-import java.time.LocalDate;
+import java.io.IOException;
 import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
+import sws.murcs.debug.errorreporting.ErrorReporter;
 import sws.murcs.model.Effort;
 import sws.murcs.model.Person;
 
@@ -15,30 +15,11 @@ import sws.murcs.model.Person;
  * Controller for logging effort on a task.
  */
 public class EffortController {
-
-    /**
-     * The person choice box.
-     */
-    @FXML
-    private ComboBox personChoiceBox;
-
-    /**
-     * The date picker for setting when this effort was spent.
-     */
-    @FXML
-    private DatePicker datePicker;
-
-    /**
-     * Effort and description text fields.
-     */
-    @FXML
-    private TextField effortTextField, descriptionTextField;
-
     /**
      * The VBox that contains all effort entries.
      */
     @FXML
-    private VBox effortsVBox;
+    private VBox effortsVBox, contentVBox;
 
     /**
      * The parent editor of this editor.
@@ -46,11 +27,20 @@ public class EffortController {
     private TaskEditor parentEditor;
 
     /**
-     * The list of people who are allowed to log time for this task.
+     * The eligible workers.
      */
     private List<Person> eligibleWorkers;
 
-    private Effort effort;
+    /**
+     * The creation effort editor.
+     */
+    private EffortEntryController createController;
+
+    /**
+     * The loader for effort entries.
+     */
+    private FXMLLoader effortEntryControllerLoader =
+            new FXMLLoader(EffortController.class.getResource("/sws/murcs/EffortEntry.fxml"));
 
     /**
      * Sets up the effort controller.
@@ -58,11 +48,36 @@ public class EffortController {
      * @param people A list of people allowed to log time for this task
      */
     public void setUp(final TaskEditor parent, final List<Person> people) {
-        effort = new Effort();
-
         parentEditor = parent;
-        datePicker.setValue(LocalDate.now());
         eligibleWorkers = people;
+
+        createController = newEffortEntryController();
+        createController.setEffort(new Effort());
+
+        contentVBox.getChildren().add(0, createController.getRoot());
+    }
+
+    private EffortEntryController newEffortEntryController() {
+        try {
+            effortEntryControllerLoader.setRoot(null);
+            effortEntryControllerLoader.setController(null);
+            Parent root = effortEntryControllerLoader.load();
+            EffortEntryController controller = effortEntryControllerLoader.getController();
+            controller.setEffortController(this);
+            controller.setRoot(root);
+            return controller;
+        }catch (IOException e) {
+            ErrorReporter.get().reportErrorSecretly(e, "Couldn't load EffortEntryController :'(");
+        }
+        return null;
+    }
+
+    /**
+     * Gets eligible workers for this task.
+     * @return The eligible workers.
+     */
+    public List<Person> getEligibleWorkers() {
+        return eligibleWorkers;
     }
 
     /**
@@ -71,6 +86,17 @@ public class EffortController {
      */
     @FXML
     private void addButtonClick(final ActionEvent event) {
-        //parentEditor.getTask().logEffort(effort);
+        //Perhaps we should grey out the add button?
+        if (createController.hasErrors()) return;
+
+        parentEditor.getTask().logEffort(createController.getEffort());
+
+        EffortEntryController controller = newEffortEntryController();
+        controller.setEffort(createController.getEffort());
+
+        //Clear the creation controller.
+        createController.setEffort(new Effort());
+
+        effortsVBox.getChildren().add(0, controller.getRoot());
     }
 }
---
+++ src/main/java/sws/murcs/controller/editor/EffortEntryController.java
@@ -0,0 +1,119 @@
+package sws.murcs.controller.editor;
+
+import java.time.LocalDate;
+import java.util.List;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sws.murcs.model.Effort;
+import sws.murcs.model.Person;
+
+/**
+ * A controller for editing effort entries
+ */
+public class EffortEntryController {
+    /**
+     * The text field containing time spent.
+     */
+    @FXML
+    private TextField timeTextField;
+
+    /**
+     * The description text area.
+     */
+    @FXML
+    private TextArea descriptionTextArea;
+
+    /**
+     * The date picker.
+     */
+    @FXML
+    private DatePicker datePicker;
+
+    /**
+     * The choice box for deciding who logged the time.
+     */
+    @FXML
+    private ComboBox personComboBox;
+
+    /**
+     * The root node of this editor.
+     */
+    private Parent root;
+
+    /**
+     * The associated effort controller.
+     */
+    private EffortController effortController;
+
+    /**
+     * The effort being edited by this controller.
+     */
+    private Effort effort;
+
+    /**
+     * Initializes the editor.
+     */
+    @FXML
+    private void initialize() {
+        datePicker.setValue(LocalDate.now());
+    }
+
+    /**
+     * Sets the root node for this editor.
+     * @param root The root node for this editor.
+     */
+    void setRoot(final Parent root) {
+        this.root = root;
+    }
+
+    /**
+     * Gets the root for this editor.
+     * @return The root node for this editor.
+     */
+    public Parent getRoot() {
+        return root;
+    }
+
+    /**
+     * Sets the effort this controller is editing.
+     * @param effort The effort for this controller to edit.
+     */
+    public void setEffort(final Effort effort) {
+        this.effort = effort;
+    }
+
+    /**
+     * The effort this controller edits.
+     * @return The effort.
+     */
+    public Effort getEffort() {
+        return effort;
+    }
+
+    /**
+     * Sets the effort controller.
+     * @param controller The effort controller.
+     */
+    public void setEffortController(EffortController controller) {
+        this.effortController = controller;
+    }
+
+    /**
+     * Indicates whether the form is valid.
+     * @return whether the form is valid.
+     */
+    public boolean hasErrors() {
+        return false;
+    }
+
+    /**
+     * Gets the eligible workers.
+     */
+    private List<Person> getEligibleWorkers() {
+        return effortController.getEligibleWorkers();
+    }
+}
