c6055bbb23d50a3acb645917dd619016bf37d075
Dion Woolley <dmw99@uclive.ac.nz>
2015-09-03 21:37:22 +1200

    #fix import

--- src/main/java/sws/murcs/controller/editor/ScrumBoard.java
+++ src/main/java/sws/murcs/controller/editor/ScrumBoard.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
