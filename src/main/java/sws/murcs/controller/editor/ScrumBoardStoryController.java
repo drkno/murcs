b99fb8ac97168c4b6abd4f480d55a691f9a4adce
Dion Woolley <dmw99@uclive.ac.nz>
2015-09-03 22:49:27 +1200

    #Implement textual descriptions of tasks in a story

--- src/main/java/sws/murcs/controller/editor/ScrumBoardStoryController.java
+++ src/main/java/sws/murcs/controller/editor/ScrumBoardStoryController.java
@@ -10,8 +10,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import sws.murcs.model.EffortEntry;
 import sws.murcs.model.Story;
 import sws.murcs.model.Task;
+import sws.murcs.model.TaskState;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 3/09/2015
@@ -42,6 +48,7 @@ public class ScrumBoardStoryController {
     public Label doneBaseInfoLabel;
     public VBox doneMoreInfoVBox;
     public AnchorPane mainPane;
+    public Label storyStateLabel;
 
     private Story story;
 
@@ -80,6 +87,79 @@ public class ScrumBoardStoryController {
         storyHyperLink.setText(story.getShortName());
         storyHyperLink.setWrapText(true);
         storyHyperLink.setOnAction(event -> sprintContainer.getNavigationManager().navigateTo(story));
+
+        setupTaskMinInfo();
+    }
+
+    /**
+     *
+     */
+    @SuppressWarnings("checkstyle:magicnumber")
+    private String calculateTime(float summary) {
+        int dps = 0;
+        String units = "minutes";
+
+        //If we have more than 60 minutes we should measure in hours.
+        if (summary >= 60) {
+            summary /= 60;
+            units = "hours";
+            dps = 1;
+        }
+
+        //To round to a certain number of dps, we multipy by 10 to the power of the dps we want
+        // 7.8934 to 2 dp: 7.8934 * 10 ^ 2 = 789.34, Round to 0 dps = 789, divide by 10 ^ 2 = 7.89
+        float pow = (float) Math.pow(10, dps);
+        summary = Math.round(summary * pow) / pow;
+
+        return summary + " " + units;
+    }
+
+    private void setupTaskMinInfo() {
+        List<Task> tasksToDo = story.getTasks()
+                .stream()
+                .filter(t -> t.getState().equals(TaskState.NotStarted))
+                .collect(Collectors.toList());
+        List<Task> tasksInProgress = story.getTasks()
+                .stream()
+                .filter(t -> t.getState().equals(TaskState.InProgress))
+                .collect(Collectors.toList());
+        List<Task> tasksDone = story.getTasks()
+                .stream()
+                .filter(t -> t.getState().equals(TaskState.Done))
+                .collect(Collectors.toList());
+
+        if (tasksToDo.size() > 0) {
+            DoubleSummaryStatistics timeLeft = tasksToDo
+                    .stream()
+                    .collect(Collectors.summarizingDouble(Task::getCurrentEstimate));
+            String[] timeLeftString = calculateTime((float) timeLeft.getSum()).split(" ");
+            toDoBaseInfoLabel.setText(tasksToDo.size() + " tasks are ready to be started\n"
+            + "with " + timeLeftString[0] + " " + timeLeftString[1] + " estimate left");
+        }
+        else {
+            toDoBaseInfoLabel.setText("No tasks left to start :)");
+        }
+
+        if (tasksInProgress.size() > 0) {
+            DoubleSummaryStatistics timeLeft = tasksInProgress
+                    .stream()
+                    .collect(Collectors.summarizingDouble(Task::getCurrentEstimate));
+            DoubleSummaryStatistics timeSpent = tasksInProgress
+                    .stream()
+                    .collect(Collectors.summarizingDouble(t -> t.getEffort()
+                            .stream()
+                            .collect(Collectors.summarizingDouble(EffortEntry::getEffort))
+                            .getSum()));
+            String[] timeLeftString = calculateTime((float) timeLeft.getSum()).split(" ");
+            String[] timeSpentString = calculateTime((float) timeSpent.getSum()).split(" ");
+            inProgressBaseInfoLabel.setText(tasksInProgress.size() + " tasks are in progress "
+                    + "with an estimated " + timeLeftString[0] + " " + timeLeftString[1] + " remaining "
+                    + "and " + timeSpentString[0] + " " + timeSpentString[1] + " spent");
+        }
+        else {
+            inProgressBaseInfoLabel.setText("No tasks are in progress");
+        }
+
     }
 
     public void setStory(Story pStory) {
