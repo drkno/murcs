f9e2ce7235e803e15b33d1e95893cb2645ed4088
Jay Harris <jay.harris@outlook.co.nz>
2015-09-14 13:46:26 +1200

    #fix fixes one I missed

--- src/main/java/sws/murcs/controller/editor/ScrumBoard.java
+++ src/main/java/sws/murcs/controller/editor/ScrumBoard.java
@@ -1,6 +1,8 @@
 package sws.murcs.controller.editor;
 
 import com.sun.javafx.css.StyleManager;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.animation.FadeTransition;
 import javafx.animation.SequentialTransition;
 import javafx.application.Platform;
@@ -13,12 +15,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import sws.murcs.debug.errorreporting.ErrorReporter;
+import sws.murcs.internationalization.AutoLanguageFXMLLoader;
 import sws.murcs.model.Sprint;
 import sws.murcs.model.Story;
 import sws.murcs.view.App;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * The Scrum Board controller. This consists of several story
