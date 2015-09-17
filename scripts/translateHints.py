b806817728abac348c94db4b80e8a605c2f3c5aa
Jay Harris <jay.harris@outlook.co.nz>
2015-09-17 13:34:01 +1200

    #implement Wrote a script that makes it possible to translate helpful hints. You can run it anytime you add new hints and it *probably* won't break anything.

---
+++ scripts/translateHints.py
@@ -0,0 +1,54 @@
+__author__ = 'jayha_000'
+
+path = "..\\src\main\\resources\\sws\\murcs\\"
+helpfulHints = path + "helpfulHints\\helpfulHints.nsv"
+englishWords = path + "languages\\words_en_EN.properties"
+
+keys = dict()
+hints = None
+
+# Function for getting a key for a hint. May need some tidying
+def key_for_hint(hint):
+	# Check if our hint has already been added but has some funky name
+	for key in keys:
+		if keys[key] == hint:
+			return key
+
+	key = hint.split("-")[-1].strip() #get the thing after the last (probably the shortcut) - to be our key
+
+	cleaned_key = ""
+	for char in key:
+		if char.lower() not in "abcdefghijklmnopqrstuvwxyz0123456789":
+			continue
+		cleaned_key += char
+
+	# If this hint has the same key as something else, give it a different key
+	while cleaned_key in keys and keys[cleaned_key] != hint:
+		cleaned_key = "_" + cleaned_key
+
+	return cleaned_key
+
+# Read all our hints
+with open(helpfulHints) as f:
+	hints = f.readlines()
+
+# Read all our keys and their english words
+with open(englishWords) as f:
+	lines = f.readlines()
+
+	for line in lines:
+		parts = line.strip('\r\n\t').split(" = ")
+		keys[parts[0]] = parts[1]
+
+# Add all the hints we don't already have to our english file
+for hint in hints:
+	key = key_for_hint(hint)
+	if key not in keys:
+		keys[key] = hint
+
+# Save our new hints file
+with open(englishWords, 'w') as f:
+	for key in sorted(keys):
+		f.write(key + " = " + keys[key] + "\n")
+
+
