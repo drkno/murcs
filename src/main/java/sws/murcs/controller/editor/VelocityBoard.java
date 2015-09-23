c88e3b809cfcc9e75d0eed8a9ff9b78ead5dddd1
Haydon Baddock <hgb42@uclive.ac.nz>
2015-09-23 14:51:43 +1200

    Points are now indicated as final or indicatory
    story:378

--- src/main/java/sws/murcs/controller/editor/VelocityBoard.java
+++ src/main/java/sws/murcs/controller/editor/VelocityBoard.java
@@ -2,16 +2,16 @@ package sws.murcs.controller.editor;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import sws.murcs.model.*;
-import sws.murcs.model.helpers.UsageHelper;
 import sws.murcs.model.persistence.PersistenceManager;
 
-import java.util.Comparator;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.ToDoubleFunction;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -39,63 +39,82 @@ public class VelocityBoard {
     }
 
     /**
+     * Initialises the fxml elements.
+     */
+    @FXML
+    private void initialize() {
+        velocityChart.setCreateSymbols(false);
+        velocityChart.getStyleClass().add(".chart-bar { -fx-background-insets: 0,1,2; }");
+    }
+
+    /**
      * Loads the team into the velocity board.
      */
     protected void loadObject() {
-        velocityChart.getData().clear();
+        String velocitySeriesStyle = "";
+        String meanSeriesStyle = "-fx-stroke-width: 1px;";
+        String medianSeriesStyle = "";
+        String pointStyle = "";
+        String indicativePointStyle = "";
 
+        velocityChart.getData().clear();
         if (team == null) {
             throw new NullPointerException("Team is null in the velocity board");
         }
 
-        XYChart.Series<String, Double> series = new XYChart.Series<>();
-        series.setName("Sprint Velocity");
-
+        Series<String, Double> velocitySeries = new Series<>();
+        velocitySeries.setName("Sprint Velocities");
         List<Sprint> sprints = PersistenceManager.getCurrent().getCurrentModel().getSprints().stream().filter(s -> team.equals(s.getTeam())).sorted((s1, s2) -> {
             if (s1.getStartDate().isEqual(s2.getStartDate())) {
                 return 0;
             }
             return s1.getStartDate().isBefore(s2.getStartDate()) ? -1 : 1;
         }).collect(Collectors.toList());
-
-        double total = 0;
+        double sumVelocities = 0;
         for (Model model : sprints) {
             Sprint sprint = (Sprint) model;
-            double sum = 0;
+            double storyTotal = 0;
             for (Story story : sprint.getStories()) {
                 int estimate = sprint.getBacklog().getEstimateType().getEstimates().indexOf(story.getEstimate()) + 1;
                 if (estimate >= 0) {
-                    sum += estimate;
+                    storyTotal += estimate;
                 }
             }
             double days = sprint.getEndDate().toEpochDay() - sprint.getStartDate().toEpochDay() + 1;
-            Double velocity = sum / days;
-            total += velocity;
+            Double velocity = storyTotal / days;
+            sumVelocities += velocity;
             String name = sprint.getShortName();
-            series.getData().add(new XYChart.Data(name, velocity));
+            Data dataPoint = new Data(name, velocity);
+            if (sprint.getEndDate().isAfter(LocalDate.now())) {
+                dataPoint.setNode(new Circle(5, Color.GRAY));
+            }
+            else {
+                dataPoint.setNode(new Circle(5, Color.RED));
+            }
+            velocitySeries.getData().add(dataPoint);
         }
-        double averageVelocity = total / sprints.size();
-        velocityChart.getData().add(series);
+        velocityChart.getData().add(velocitySeries);
+
+        // Add the horizontal lines
         if (sprints.size() > 1) {
             String firstSprint = sprints.get(0).getShortName();
             String lastSprint = sprints.get(sprints.size() - 1).getShortName();
 
             // Get the mean velocity
-            XYChart.Series meanChart = new XYChart.Series();
-            meanChart.setName("Average Velocity");
-            meanChart.getData().add(new XYChart.Data(firstSprint, averageVelocity));
-            meanChart.getData().add(new XYChart.Data(lastSprint, averageVelocity));
-            velocityChart.getData().add(meanChart);
+            Series meanSeries = new Series();
+            double averageVelocity = sumVelocities / sprints.size();
+            meanSeries.setName("Average Velocity");
+            meanSeries.getData().add(new Data(firstSprint, averageVelocity));
+            meanSeries.getData().add(new Data(lastSprint, averageVelocity));
+            velocityChart.getData().add(meanSeries);
 
             // Get the median velocity
-            XYChart.Series<String, Double> medianChart = new XYChart.Series<>();
-            List<XYChart.Data<String, Double>> sortedVelocities = series.getData().stream().sorted((o1, o2) -> Double.compare(o1.getYValue(), o2.getYValue())).collect(Collectors.toList());
+            Series medianChart = new Series();
+            List<Data<String, Double>> sortedVelocities = velocitySeries.getData().stream().sorted((o1, o2) -> Double.compare(o1.getYValue(), o2.getYValue())).collect(Collectors.toList());
             double median = sortedVelocities.get((sortedVelocities.size() - 1) / 2).getYValue();
-
             medianChart.setName("Median Velocity");
-            medianChart.getData().add(new XYChart.Data(firstSprint, median));
-            medianChart.getData().add(new XYChart.Data(lastSprint, median));
-
+            medianChart.getData().add(new Data(firstSprint, median));
+            medianChart.getData().add(new Data(lastSprint, median));
             velocityChart.getData().add(medianChart);
         }
     }
